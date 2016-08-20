05ddd7bd59b1e71f4dc517146c615497fa4dd5e2
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2016-08-20 15:38:24 -0300

    Extract IndexHandler

---
+++ index_handler.go
@@ -0,0 +1,14 @@
+import (
+	"io/ioutil"
+	"net/http"
+)
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	file, err := ioutil.ReadFile("webapp/index.html")
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Write(file)
+}
