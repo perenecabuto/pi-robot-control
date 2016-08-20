f8e08827d7fd9fc15fcce648adb909a054924ce3
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2016-08-20 18:18:05 -0300

    Organize main

--- main.go
+++ main.go
@@ -12,33 +12,25 @@ var (
 )
 
 func main() {
-	robot, err := NewRobot(17, 27, 4, 22)
-	if err != nil {
-		log.Panic(err)
-	}
-
+	robot := NewRobot(17, 27, 4, 22)
 	robotHandler := NewRobotHandler(robot)
 	stream := mjpeg.NewStream()
 	capture := NewWebcamCapture(1000, "/dev/video0")
 
 	http.Handle("/camera", stream)
-	http.Handle("/move", robotHandler)
+	http.Handle("/move/", robotHandler)
+	http.HandleFunc("/", IndexHandler)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		file, err := ioutil.ReadFile("webapp/index.html")
+	go func() {
+		err := capture.Listen(func(frame []byte) {
+			stream.UpdateJPEG(frame)
+		})
 		if err != nil {
-			http.Error(w, err.Error(), 500)
-			return
+			log.Println(err)
 		}
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		w.Write(file)
-	})
+	}()
 
 	go robot.Initialize()
-	go capture.Listen(func(frame []byte) {
-		stream.UpdateJPEG(frame)
-	})
-
 	log.Println("Starting server on:", ServerAddress)
 	log.Panic(http.ListenAndServe(ServerAddress, nil))
 }
