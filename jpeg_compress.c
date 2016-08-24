298e8bf9f5946ea8e9db6d7e9ca823b545b064a3
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2016-08-24 01:59:15 -0300

    Implement YUYV converter

--- jpeg_compress.c
+++ jpeg_compress.c
@@ -19,71 +19,52 @@
  * inspired by: http://stackoverflow.com/questions/17029136
  * /weird-image-while-trying-to-compress-yuv-image-to-jpeg-using-libjpeg
  */
-void compressYUYVtoJPEG(unsigned char* input, unsigned char* output, const int width, const int height) {
-	struct jpeg_compress_struct cinfo;
-	struct jpeg_error_mgr jerr;
-	JSAMPROW row_pointer[1];
-	unsigned char *line_buffer, *yuyv;
-	int z = 0;
+void compressYUYVtoJPEG(const uint8_t* const input, uint8_t* output, uint32_t width, uint32_t height) {
+    struct jpeg_compress_struct cinfo;
+    struct jpeg_error_mgr jerr;
+    JSAMPROW row_ptr[1];
+    int row_stride;
 
-	uint8_t* outbuffer = NULL;
-	uint64_t outlen = 0;
+    uint8_t* outbuffer = NULL;
+    uint64_t outlen = 0;
 
-	yuyv = input;
+    cinfo.err = jpeg_std_error(&jerr);
+    jpeg_create_compress(&cinfo);
+    jpeg_mem_dest(&cinfo, &outbuffer, &outlen);
 
-	cinfo.err = jpeg_std_error(&jerr);
-	jpeg_create_compress(&cinfo);
-	jpeg_mem_dest(&cinfo, &outbuffer, &outlen);
+    // jrow is a libjpeg row of samples array of 1 row pointer
+    cinfo.image_width = width & -1;
+    cinfo.image_height = height & -1;
+    cinfo.input_components = 3;
+    cinfo.in_color_space = JCS_YCbCr; //libJPEG expects YUV 3bytes, 24bit
 
-	// jrow is a libjpeg row of samples array of 1 row pointer
-	cinfo.image_width = width & -1;
-	cinfo.image_height = height & -1;
-	cinfo.input_components = 3;
-	cinfo.in_color_space = JCS_YCbCr; //libJPEG expects YUV 3bytes, 24bit
-	line_buffer = calloc(cinfo.image_width * 3, 1);
+    jpeg_set_defaults(&cinfo);
+    jpeg_set_quality(&cinfo, 100, TRUE);
+    jpeg_start_compress(&cinfo, TRUE);
 
-	jpeg_set_defaults(&cinfo);
-	jpeg_set_quality(&cinfo, 100, TRUE);
-	jpeg_start_compress(&cinfo, TRUE);
+    JSAMPROW row_pointer[1];
+    uint8_t* tmprowbuf = malloc(width * 3 * sizeof(uint8_t));
+    row_pointer[0] = &tmprowbuf[0];
 
-	while(cinfo.next_scanline < cinfo.image_height) {
-		int x;
-		unsigned char *ptr = line_buffer;
+    while (cinfo.next_scanline < cinfo.image_height) {
+        unsigned i, j;
+        unsigned offset = cinfo.next_scanline * cinfo.image_width * 2; //offset to the correct row
+        for (i = 0, j = 0; i < cinfo.image_width * 2; i += 4, j += 6) { //input strides by 4 bytes, output strides by 6 (2 pixels)
+            tmprowbuf[j + 0] = input[offset + i + 0]; // Y (unique to this pixel)
+            tmprowbuf[j + 1] = input[offset + i + 1]; // U (shared between pixels)
+            tmprowbuf[j + 2] = input[offset + i + 3]; // V (shared between pixels)
+            tmprowbuf[j + 3] = input[offset + i + 2]; // Y (unique to this pixel)
+            tmprowbuf[j + 4] = input[offset + i + 1]; // U (shared between pixels)
+            tmprowbuf[j + 5] = input[offset + i + 3]; // V (shared between pixels)
+        }
+        jpeg_write_scanlines(&cinfo, row_pointer, 1);
+    }
 
-		for (x = 0; x < cinfo.image_width; x++) {
-			int r, g, b;
-			int y, u, v;
+    jpeg_finish_compress(&cinfo);
+    jpeg_destroy_compress(&cinfo);
+    free(tmprowbuf);
 
-			if (!z)
-				y = yuyv[1] << 8;
-			else
-				y = yuyv[3] << 8;
-			u = yuyv[0] - 128;
-			v = yuyv[2] - 128;
-
-			r = (y + (359 * v)) >> 8;
-			g = (y - (88 * u) - (183 * v)) >> 8;
-			b = (y + (454 * u)) >> 8;
-
-			*(ptr++) = (r > 255) ? 255 : ((r < 0) ? 0 : r);
-			*(ptr++) = (g > 255) ? 255 : ((g < 0) ? 0 : g);
-			*(ptr++) = (b > 255) ? 255 : ((b < 0) ? 0 : b);
-
-			if (z++) {
-				z = 0;
-				yuyv += 4;
-			}
-		}
-
-		row_pointer[0] = line_buffer;
-		jpeg_write_scanlines(&cinfo, row_pointer, 1);
-	}
-
-	jpeg_finish_compress(&cinfo);
-	jpeg_destroy_compress(&cinfo);
-	free(line_buffer);
-
-	/*printf("libjpeg produced %d bytes %d\n", outlen, cinfo.image_width);*/
-	strncpy(output, outbuffer, outlen);
+    /*printf("libjpeg produced %d bytes\n", outlen);*/
+    memcpy(output, outbuffer, outlen);
 }
 
