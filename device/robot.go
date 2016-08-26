08c9a452e7bba28e8ccdbd18a7e892f3a1809d60
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2016-08-26 03:48:49 -0300

    Add autostop

--- device/robot.go
+++ device/robot.go
@@ -1,14 +1,19 @@
 package device
 
 import (
+	"time"
+
 	"github.com/stianeikeland/go-rpio"
 )
+
+const ACTION_TIMEOUT_SEC = 5
 
 type Robot struct {
 	pin1F     rpio.Pin
 	pin2F     rpio.Pin
 	pin1B     rpio.Pin
 	pin2B     rpio.Pin
+	stopTimer *time.Timer
 }
 
 func NewRobot(gpio1F, gpio2F, gpio1B, gpio2B uint8) *Robot {
@@ -16,8 +21,9 @@ func NewRobot(gpio1F, gpio2F, gpio1B, gpio2B uint8) *Robot {
 	pin2F := rpio.Pin(gpio2F)
 	pin1B := rpio.Pin(gpio1B)
 	pin2B := rpio.Pin(gpio2B)
+	timer := time.NewTimer(ACTION_TIMEOUT_SEC)
 
-	return &Robot{pin1F, pin2F, pin1B, pin2B}
+	return &Robot{pin1F, pin2F, pin1B, pin2B, timer}
 }
 
 func (r Robot) Initialize() error {
@@ -63,10 +69,18 @@ func (r Robot) Stop() {
 }
 
 func (r Robot) move(val1F, val2F, val1B, val2B bool) {
+	go r.autoStop()
 	toggle(r.pin1F, val1F)
 	toggle(r.pin2F, val2F)
 	toggle(r.pin1B, val1B)
 	toggle(r.pin2B, val2B)
+}
+
+func (r Robot) autoStop() {
+	if !r.stopTimer.Stop() {
+		<-r.stopTimer.C
+	}
+	r.stopTimer.Reset(ACTION_TIMEOUT_SEC)
 }
 
 func toggle(pin rpio.Pin, on bool) {
