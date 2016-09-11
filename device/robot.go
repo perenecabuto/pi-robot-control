d39cf743453e9a14a57bc2a25840e28ae2712ca8
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2016-09-10 22:19:00 -0300

    Change implementation to embd

--- device/robot.go
+++ device/robot.go
@@ -1,41 +1,45 @@
 package device
 
 import (
+	"log"
 	"time"
 
-	"github.com/stianeikeland/go-rpio"
+	"github.com/kidoman/embd"
+	_ "github.com/kidoman/embd/host/rpi"
 )
 
 const ACTION_TIMEOUT_SEC = 5
 
 type Robot struct {
-	pin1F     rpio.Pin
-	pin2F     rpio.Pin
-	pin1B     rpio.Pin
-	pin2B     rpio.Pin
+	pin1F     embd.DigitalPin
+	pin2F     embd.DigitalPin
+	pin1B     embd.DigitalPin
+	pin2B     embd.DigitalPin
 	stopTimer *time.Timer
 }
 
-func NewRobot(gpio1F, gpio2F, gpio1B, gpio2B uint8) *Robot {
-	pin1F := rpio.Pin(gpio1F)
-	pin2F := rpio.Pin(gpio2F)
-	pin1B := rpio.Pin(gpio1B)
-	pin2B := rpio.Pin(gpio2B)
-	timer := time.NewTimer(ACTION_TIMEOUT_SEC)
+func NewRobot(gpio1F, gpio2F, gpio1B, gpio2B int) *Robot {
+	pins := make([]embd.DigitalPin, 4, 4)
+	for i, gpio := range []int{gpio1F, gpio2F, gpio1B, gpio2B} {
+		pins[i], _ = embd.NewDigitalPin(gpio)
+	}
 
-	return &Robot{pin1F, pin2F, pin1B, pin2B, timer}
+	timer := time.NewTimer(ACTION_TIMEOUT_SEC)
+	return &Robot{pins[0], pins[1], pins[2], pins[3], timer}
 }
 
 func (r Robot) Initialize() error {
-	err := rpio.Open()
-	if err != nil {
+	r.CleanUP()
+	if err := embd.InitGPIO(); err != nil {
 		return err
 	}
 
-	r.pin1F.Output()
-	r.pin2F.Output()
-	r.pin1B.Output()
-	r.pin2B.Output()
+	for _, pin := range r.Pins() {
+		if err := pin.SetDirection(embd.Out); err != nil {
+			return err
+		}
+	}
+
 	r.Stop()
 
 	go func() {
@@ -68,6 +72,20 @@ func (r Robot) Stop() {
 	r.move(false, false, false, false)
 }
 
+func (r Robot) CleanUP() {
+	log.Println("Deactivating robot...")
+	for _, pin := range r.Pins() {
+		if err := pin.Close(); err != nil {
+			log.Println("CleanUP ERROR - close:", err.Error())
+		}
+	}
+	embd.CloseGPIO()
+}
+
+func (r Robot) Pins() []embd.DigitalPin {
+	return []embd.DigitalPin{r.pin1F, r.pin1B, r.pin2F, r.pin2B}
+}
+
 func (r Robot) move(val1F, val2F, val1B, val2B bool) {
 	go r.autoStop()
 	toggle(r.pin1F, val1F)
@@ -83,10 +101,12 @@ func (r Robot) autoStop() {
 	r.stopTimer.Reset(ACTION_TIMEOUT_SEC)
 }
 
-func toggle(pin rpio.Pin, on bool) {
+func toggle(pin embd.DigitalPin, on bool) {
+	io := embd.Low
 	if on {
-		pin.High()
-	} else {
-		pin.Low()
+		io = embd.High
+	}
+	if err := pin.Write(io); err != nil {
+		log.Println("Error:", err.Error())
 	}
 }
