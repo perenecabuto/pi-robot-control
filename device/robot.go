a1a1e08d26f58c1a00ec7b536c62051e69bbe542
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2016-10-23 20:18:20 -0200

    Move Robot gpio controls to new MotorControls

--- device/robot.go
+++ device/robot.go
@@ -1,86 +1,73 @@
 package device
 
 import (
-	"time"
+	"fmt"
+	"log"
+	"os"
 
 	"github.com/stianeikeland/go-rpio"
 )
 
-const ACTION_TIMEOUT_SEC = 5
-
 type Robot struct {
-	pin1F     rpio.Pin
-	pin2F     rpio.Pin
-	pin1B     rpio.Pin
-	pin2B     rpio.Pin
-	stopTimer *time.Timer
+	Move *MotorController
 }
 
-func NewRobot(gpio1F, gpio2F, gpio1B, gpio2B uint8) *Robot {
+type MotorController struct {
+	pin1F rpio.Pin
+	pin2F rpio.Pin
+	pin1B rpio.Pin
+	pin2B rpio.Pin
+}
+
+func NewMotorController(gpio1F, gpio2F, gpio1B, gpio2B uint8) *MotorController {
 	pin1F := rpio.Pin(gpio1F)
 	pin2F := rpio.Pin(gpio2F)
 	pin1B := rpio.Pin(gpio1B)
 	pin2B := rpio.Pin(gpio2B)
-	timer := time.NewTimer(ACTION_TIMEOUT_SEC)
 
-	return &Robot{pin1F, pin2F, pin1B, pin2B, timer}
+	return &MotorController{pin1F, pin2F, pin1B, pin2B}
 }
 
-func (r Robot) Initialize() error {
+func (c MotorController) Initialize() error {
 	err := rpio.Open()
 	if err != nil {
 		return err
 	}
 
-	r.pin1F.Output()
-	r.pin2F.Output()
-	r.pin1B.Output()
-	r.pin2B.Output()
-	r.Stop()
-
-	go func() {
-		for range r.stopTimer.C {
-			r.stopTimer.Stop()
-			r.Stop()
-		}
-	}()
+	c.pin1F.Output()
+	c.pin2F.Output()
+	c.pin1B.Output()
+	c.pin2B.Output()
+	c.Stop()
 
 	return nil
 }
 
-func (r Robot) Forward() {
-	r.move(true, true, false, false)
+func (c MotorController) Forward() {
+	c.move(true, true, false, false)
 }
 
-func (r Robot) Backward() {
-	r.move(false, false, true, true)
+func (c MotorController) Backward() {
+	c.move(false, false, true, true)
 }
 
-func (r Robot) Left() {
-	r.move(true, false, false, true)
+func (c MotorController) Left() {
+	c.move(true, false, false, true)
 }
 
-func (r Robot) Right() {
-	r.move(false, true, true, false)
+func (c MotorController) Right() {
+	c.move(false, true, true, false)
 }
 
-func (r Robot) Stop() {
-	r.move(false, false, false, false)
+func (c MotorController) Stop() {
+	c.move(false, false, false, false)
 }
 
-func (r Robot) move(val1F, val2F, val1B, val2B bool) {
-	go r.autoStop()
-	toggle(r.pin1F, val1F)
-	toggle(r.pin2F, val2F)
-	toggle(r.pin1B, val1B)
-	toggle(r.pin2B, val2B)
-}
-
-func (r Robot) autoStop() {
-	if !r.stopTimer.Stop() {
-		<-r.stopTimer.C
-	}
-	r.stopTimer.Reset(ACTION_TIMEOUT_SEC)
+func (c MotorController) move(val1F, val2F, val1B, val2B bool) {
+	toggle(c.pin1F, val1F)
+	toggle(c.pin2F, val2F)
+	toggle(c.pin1B, val1B)
+	toggle(c.pin2B, val2B)
 }
 
 func toggle(pin rpio.Pin, on bool) {
